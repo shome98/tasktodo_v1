aaf6a0ab1450d4c1bcc120c427801399aeeb4f48
Sowparna Shome <soupornoshome@gmail.com>
2025-03-04 18:59:12 +0530

    get todos

---
+++ app/api/todos/route.ts
@@ -0,0 +1,50 @@
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+const ITEMS_PER_PAGE = 10;
+
+export async function GET(req: NextRequest) {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return NextResponse.json({ error: "😠Unauthorized" }, { status: 401 });
+    }
+
+    const { searchParams } = new URL(req.url);
+    const page = parseInt(searchParams.get("page") || "1");
+    const search = searchParams.get("search") || "";
+
+    try {
+        const todos = await prisma.todo.findMany({
+            where: { userId, title: { contains: search, mode: "insensitive" } },
+            orderBy: { createdAt: "desc" },
+            take: ITEMS_PER_PAGE,
+            skip: (page - 1) * ITEMS_PER_PAGE
+        });
+        const totalItems = await prisma.todo.count({
+            where: {
+                userId,
+                title: {
+                    contains: search,
+                    mode: "insensitive",
+                },
+            },
+        });
+
+        const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
+
+        return NextResponse.json({
+            todos,
+            currentPage: page,
+            totalPages,
+        });
+    }
+    catch (error) {
+        console.error("❌Error getting todos:", error);
+        return NextResponse.json(
+            { error: "🚫Internal server error" },
+            { status: 500 }
+        );
+    }
+}
